901c6b2fbe4bef94b4b580ea0ad18c3e05b97d48
Orekhov Mikhail <morekhov@users.noreply.github.com>
2023-12-14 12:00:12 +0300

    Added: docs.

---
+++ readme.md
@@ -0,0 +1,9 @@
+# dji-msdk-issues
+
+This project contains a set of small [Android instrumentation tests](https://source.android.com/docs/core/tests/development/instrumentation) 
+that explain issues in DJI MSDK 5.
+
+To run tests open project in Android Studio 2022 (or newer), connect DJI RC Plus to the laptop
+via USB, select the RC in the Android studio device list, run the test.
+
+Also see test description for additional preconditions and instructions before run in.
--- src/androidTest/java/com/sph/diagnostics/dji/ReadDataFromPayload.kt
+++ src/androidTest/java/com/sph/diagnostics/dji/ReadDataFromPayload.kt
@@ -18,27 +18,49 @@ import kotlinx.coroutines.withTimeout
 import org.junit.Test
 
 import org.slf4j.LoggerFactory
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Duration.Companion.minutes
 
 class ReadDataFromPayload {
     /**
      * This test explains the issue when MSDK doesn't receive data from the payload.
      *
+     * Hardware configuration:
+     * - The payload is an ARM64 onboard computer that is connected to DJI M350 via UART.
+     * - The payload hosts a simple application that that does nothing but sends
+     * some small data to the mobile application through DJI PSDK every 1 second.
      *
-     * Common preconditions:
-     * - The payload is an onboard computer that is connected to DJI M350 via UART;
-     * - The payload hosts a simple PSDK application that does nothing but sends
-     * some small data every 1 second.
+     * SCENARIO 1 (success).
      *
-     * SCENARIO 1 (success) - test receives data successfully.
-     * Preconditions: the payload is switched on.
-     * Scenario: Run the test, wait for the result.
-     * Result: the test received data from the payload, test passes.
+     * Preconditions:
+     *      1. The drone is switched off;
+     *      2. The RC is switched off;
+     *      3. The Payload is connected to the drone and switched off.
      *
-     * SCENARIO 2 (failure) - test does not receive any data from the payload.
-     * Preconditions: the payload is switched off
-     * Scenario: Run the test, switch on the payload, wait for the result.
-     * Result: Test fails as there is no data received from the payload.
+     * Steps:
+     *      1. Switch on the drone and the payload, wait until the drone becomes initialized.
+     *      2. When the drone is initialized switch on the RC.
+     *      3. Run the test, wait for the result.
+     *
+     * Result: the test passed as it received data from the payload.
+     *
+     *
+     * SCENARIO 2 (failure).
+     *
+     * * Preconditions:
+     *      1. The drone is switched off;
+     *      2. The RC is switched off;
+     *      3. The Payload is connected to the drone and switched off.
+     *
+     *  Steps:
+     *      1. Switch on the RC, wait until RC becomes running.
+     *      2. Run the test.
+     *      2. Switch on the drone and the payload.
+     *      3. Wait for the test result.
+     *
+     * Result: The test fails as there is no data received from the payload
+     *      during 2 minutes (2 minutes is enough to get the drone initialized).
+     *
+     * Expected result: the test receive some data from the payload.
      */
     @Test
     fun mobileAppStartedFirst(): Unit = runBlocking {
@@ -46,7 +68,7 @@ class ReadDataFromPayload {
 
         awaitAircraft()
 
-        // The payload is connected to the EXTERNAL port as it is an onboard computer
+        // The payload is connected to the EXTERNAL port
         val pm = PayloadCenter.getInstance().payloadManager[PayloadIndexType.EXTERNAL]!!
 
         awaitPayload(pm)
@@ -64,13 +86,13 @@ class ReadDataFromPayload {
         }
 
         // Waiting for any data from the payload, up to 30 seconds.
-        withTimeout(30.seconds) {
+        withTimeout(2.minutes) {
             dataChannel.receive()
         }
     }
 
     /**
-     * Returns as soon as any payload is being connected on the payload manager.
+     * Returns as soon as any payload is being connected on the passed payload manager.
      */
     private suspend fun awaitPayload(pm: IPayloadManager) {
         pm.listenPayloadBasicInfo().first { it.isConnected }
